e26cce36bb6c3e256d45be360420cdc94294263c
zrz116 <51958665+zrz116@users.noreply.github.com>
2019-06-23 15:08:09 +0800

    Add files via upload

---
+++ insertion_sort.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+
+def insertion_sort(list):
+    N = len(list)
+    for j in range(1, N):
+        for i in range(0, j):
+            if list[j] < list[i]:
+                t = list[i]
+                list[i] = list[j]
+                list[j] = t
+    return list
+
+list = [5, 3, 1, 2, 4]
+print(insertion_sort(list))
